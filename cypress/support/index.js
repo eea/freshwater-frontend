6405f3e689569df2906651e48445099734ee5555
kreafox <krisztina.n.elekes@gmail.com>
2021-10-19 12:37:07 +0300

    Revert cypress change

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -37,7 +37,6 @@ export const setupBeforeEach = () => {
     contentTitle: 'My Page',
     path: 'cypress',
   });
-  cy.wait(500);
   cy.visit('/cypress/my-page');
   cy.waitForResourceToLoad('@navigation');
   cy.waitForResourceToLoad('@breadcrumbs');
