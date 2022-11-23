ef1c26711e95fe7b74566d301a5e4c2d8bdfc9af
Valentin Dumitru <valentin.dumitru@eaudeweb.ro>
2022-11-23 15:29:40 +0200

    fix public_path

--- entrypoint-dev.sh
+++ entrypoint-dev.sh
@@ -3,13 +3,19 @@ set -Ex
 
 function apply_path {
     mainjs=./build/server.js
-    bundlejs=./build/public/static/js/*.js
+    bundlejs=./build/public/static/js/*
+    bundlecss=./build/public/static/css/*
+    bundlejson=./build/*.json
     test -f $mainjs
 
     echo "Check that we have API_PATH and API vars"
     test -n "$RAZZLE_API_PATH"
 
     echo "Changing built files inplace"
+    sed -i "s#VOLTO_PUBLIC_PATH#${PUBLIC_PATH}#g" $mainjs
+    sed -i "s#VOLTO_PUBLIC_PATH#${PUBLIC_PATH}#g" $bundlejs
+    sed -i "s#VOLTO_PUBLIC_PATH#${PUBLIC_PATH}#g" $bundlecss
+    sed -i "s#VOLTO_PUBLIC_PATH#${PUBLIC_PATH}#g" $bundlejson
     sed -i "s#VOLTO_API_PATH#${RAZZLE_API_PATH}#g" $mainjs
     sed -i "s#VOLTO_API_PATH#${RAZZLE_API_PATH}#g" $bundlejs
     sed -i "s#VOLTO_INTERNAL_API_PATH#${RAZZLE_INTERNAL_API_PATH}#g" $mainjs
