0cc11b4b619cda76dc3efd48eee0bc290f93790c
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-10-20 08:45:20 +0300

    Redo some tests

--- livetest/tests/freshwater.spec.js
+++ livetest/tests/freshwater.spec.js
@@ -3,35 +3,26 @@ import { test, expect } from '@playwright/test'
 const site = 'https://water.europa.eu/freshwater'
 
 const url = path => `${site}${path}`
+const _10s = 10000
 
 test.use({
   viewport: { width: 1600, height: 1200 }
 })
 
-test('has title', async ({ page }) => {
-  await page.goto(url('/'))
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/WISE Freshwater/)
-})
-
 test('homepage', async ({ page }) => {
   await page.goto(url('/'))
 
-  await expect(page).toHaveScreenshot({ fullPage: true })
+  await expect(page).toHaveTitle(/WISE Freshwater/)
+  await expect(page).toHaveScreenshot({ fullPage: true, timeout: _10s })
 })
 
 test('about', async ({ page }) => {
   await page.goto(url('/about/wise-freshwater'))
-  await expect(page).toHaveScreenshot({ fullPage: true })
-})
-
-test('about page, relevant links', async ({ page }) => {
-  await page.goto(url('/about/wise-freshwater'))
+  await expect(page).toHaveScreenshot('about.png', { fullPage: true, timeout: _10s })
 
   const relevantLinksH3 = page.getByText('Relevant Links')
   const box = page.locator('css=.columns-view').filter({ has: relevantLinksH3 })
-  await expect(box).toHaveScreenshot()
+  await expect(box).toHaveScreenshot('about-relevant-links.png')
 })
 
 test('eu country page', async ({ page }) => {
@@ -45,10 +36,10 @@ test('eu country page', async ({ page }) => {
   // await expect(page).toHaveScreenshot('eu-page2.png', { timeout: 300000, maxDiffPixels: 400 })
 
   await page.keyboard.press('PageDown')
-  await expect(page).toHaveScreenshot('eu-page3.png', { timeout: 10000 })
+  await expect(page).toHaveScreenshot('eu-page3.png', { timeout: _10s })
 
   await page.keyboard.press('PageDown')
-  await expect(page).toHaveScreenshot('eu-page4.png', { timeout: 10000 })
+  await expect(page).toHaveScreenshot('eu-page4.png', { timeout: _10s })
 
   // await expect(page).toHaveScreenshot({ fullPage: true, timeout: 200000 })
 })
