b6828c2269908cbc74c4e83bd739016b97076a15
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-10-28 14:55:53 +0300

    Update

--- livetest/tests/freshwater.spec.js
+++ livetest/tests/freshwater.spec.js
@@ -1,4 +1,5 @@
 import { test, expect } from '@playwright/test'
+import AxeBuilder from '@axe-core/playwright' // 1
 
 const site = 'https://water.europa.eu/freshwater'
 
@@ -16,6 +17,9 @@ test('homepage', async ({ page }) => {
 
   await expect(page).toHaveTitle(/WISE Freshwater/)
   await expect(page).toHaveScreenshot({ fullPage: true, timeout: _10s })
+
+  // const accessibilityScanResults = await new AxeBuilder({ page }).analyze()
+  // expect(accessibilityScanResults.violations).toEqual([])
 })
 
 test('about', async ({ page }) => {
@@ -25,6 +29,9 @@ test('about', async ({ page }) => {
   const relevantLinksH3 = page.getByText('Relevant Links')
   const box = page.locator('css=.columns-view').filter({ has: relevantLinksH3 })
   await expect(box).toHaveScreenshot('about-relevant-links.png')
+
+  // const accessibilityScanResults = await new AxeBuilder({ page }).analyze()
+  // expect(accessibilityScanResults.violations).toEqual([])
 })
 
 test('eu country page', async ({ page }) => {
@@ -42,6 +49,16 @@ test('eu country page', async ({ page }) => {
 
   await page.keyboard.press('PageDown')
   await expect(page).toHaveScreenshot('eu-page4.png', { timeout: _10s })
+})
 
-  // await expect(page).toHaveScreenshot({ fullPage: true, timeout: 200000 })
+test('eea accessibility page', async ({ page }) => {
+  await page.goto('https://www.eea.europa.eu/en/accessibility/tests')
+  // await expect(page).toHaveScreenshot('about.png', { fullPage: true, timeout: _10s })
+  //
+  // const relevantLinksH3 = page.getByText('Relevant Links')
+  // const box = page.locator('css=.columns-view').filter({ has: relevantLinksH3 })
+  // await expect(box).toHaveScreenshot('about-relevant-links.png')
+
+  const accessibilityScanResults = await new AxeBuilder({ page }).analyze()
+  expect(accessibilityScanResults.violations).toEqual([])
 })
